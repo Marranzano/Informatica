6c33dde12738cce89f5e2e7619ed050754f152e6
Marranzano <leomarranzano@gmail.com>
2024-12-16 10:33:01 +0000

    update

--- D - Istruzioni decisionali e iterative/for/f_primi.c
+++ D - Istruzioni decisionali e iterative/for/f_primi.c
@@ -1,14 +1,27 @@
+#include <stdio.h>
+#define N 100
+
 int main()
 {
-    int cont,num=1,2cont=1,div;
-    cont=0;
-
-
-
-
-
-
-
-
-
+    int num, i, num_primo;
+    
+    for(num = 2; num <= N; num++)  // Iniziamo da 2, poiché 1 non è considerato un numero primo
+    {
+        num_primo = 1;  // Supponiamo che il numero sia primo
+        
+        for(i = 2; i * i <= num; i++)  // Verifica divisori fino alla radice quadrata di num
+        {
+            if(num % i == 0)  // Se il numero è divisibile per i, non è primo
+            {
+                num_primo = 0;  // Non è primo
+            }
+        }
+        
+        if(num_prim)  // Se il numero è primo
+        {
+            printf("%d \n", num);
+        }
+    }
+    
+    return 0;
 }
