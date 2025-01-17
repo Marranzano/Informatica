fcf2e648775316230a3c6a365a16bba5fdacbc1e
Marranzano <leomarranzano@gmail.com>
2025-01-17 11:00:51 +0000

    update

---
+++ D - Istruzioni decisionali e iterative/funzioni/media_tra_due_nr.c
@@ -0,0 +1,51 @@
+//media tra due nr uno scelto da un'utente e uno scelto dal computer e la media tra due costanti
+#include <stdio.h>
+
+#define c 8
+#define d 6
+
+float media(int x, int y);
+int fattoriale(int x);
+int MCD(int x, int y);
+int Potenza(int x);
+
+int main()
+{
+    float k;
+    int a=5,b;
+        // test funzione media 
+    /*printf("dammi un numero\n");
+    scanf("%d",&b);
+
+    k=media(a,b);
+    printf("la media tra due nr uno scelto da un'utente e uno scelto dal computer quello scelto dal computer e 5 e'%.1f\n",k);
+
+    k=media(c,d);
+    printf("la media tra due costanti che sono 8 e 6 e'%.1f\n",k);
+
+    k=media(c,4);
+    printf("la media tra una costante e un nr scelto dal computer che sono 8 e 4 e'%.1f\n",k);
+
+    k=media(8,0);
+    printf("la media tra due nr scelti dal computer che sono 8 e 0 e'%.1f\n",k);
+
+    k=media(-5,2);
+    printf("la media tra due nr scelti dal computer che sono -5 e 2 e'%.1f\n",k);
+    */
+
+    //test fattoriale chiesto all'utente 5 volte chiede un nr nel for e esegue il fattoriale
+    int k;
+}
+
+float media(int x, int y)
+{
+    float m;
+    m=(x+y)/2.0;
+    return m; 
+}
+int fattoriale
+{
+
+
+
+}
