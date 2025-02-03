aaff084f3d89826614bcf1c76f5601d5a7b17de1
Marranzano <leomarranzano@gmail.com>
2025-02-03 10:17:34 +0000

    update

---
+++ D - Istruzioni decisionali e iterative/funzioni/es01b/somma_cifre.c
@@ -0,0 +1,8 @@
+//Scrivi una funzione che trovi la somma delle cifre di un dato numero intero. Ad esempio, la somma delle cifre di 123 è 6.
+#include <stdio.h>
+int somma_cifre(int x);
+int main()
+{
+
+
+}
