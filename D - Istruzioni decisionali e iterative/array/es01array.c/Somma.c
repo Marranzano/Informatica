98d0261c5b04db2d797d6cff2ff89446cea9a12e
Marranzano <leomarranzano@gmail.com>
2025-03-10 10:26:36 +0000

    es billi

---
+++ D - Istruzioni decisionali e iterative/array/es01array.c/Somma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+float array[10];
+int main(){
+float somma;
+float media;
+for (int i= 0; i <10; i++)
+{
+ printf("dammi un nr");
+ scanf("%f",&array[i]);
+ somma+=array[i];
+ media=somma/10.0;
+}
+printf("la media e' %f e la somma e' %f",media,somma);
+return 0;
+}
