6aa89833c15c4b233328b499ab3be473be538419
Marranzano <leomarranzano@gmail.com>
2025-03-10 11:00:33 +0000

    d

--- D - Istruzioni decisionali e iterative/array/es01array.c/ricerca.c
+++ D - Istruzioni decisionali e iterative/array/es01array.c/ricerca.c
@@ -4,13 +4,33 @@ Chieda all'utente un numero da cercare
 Verifichi se il numero è presente nell'array
 Stampi la posizione del numero se presente, altrimenti un messaggio appropriato */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(){
-int array[15];
 
-for (int i= 0; i <10; i++)
+int main() {
+    int vet[21];
+
+    // inizializzazione del generatore di numeri casuali
+    // altrimenti i numeri generati saranno sempre gli stessi
+    srand(time(NULL)); 
+
+    // inizializzo il vettore con numeri casuali
+   
+    printf("\n");
+    for (int i = 1; i < 21; i++) {
+        //generiamo un numero random tra 0 e 9 compresi
+        vet[i] = rand() % 21;
+    }   
+printf("dimmi un nr");
+scanf("%d",);
+for (int i=0; i<1;i++)
 {
- printf("%d", array[i]);
+if 
+
 }
-return 0;
+
+
+
 }
+
