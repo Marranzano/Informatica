1c7fa88c93ed581647c680c65536000aa789a0bf
Marranzano <leomarranzano@gmail.com>
2025-03-17 10:25:20 +0000

    ma

--- D - Istruzioni decisionali e iterative/array/es01array.c/a/inizializzazione.c
+++ D - Istruzioni decisionali e iterative/array/es01array.c/a/inizializzazione.c
@@ -6,10 +6,10 @@ Visualizzi il contenuto dell'array */
 #include <stdio.h>
 
 int main(){
-int array[10]={1,2,3,4,5,6,7,8,9,10}
-for (int i= 0; i <10; i++)
+int array[10]={1,2,3,4,5,6,7,8,9,10};
+for(int i= 0; i <10; i++)
 {
- printf("%d", array[i]);
+ printf("%d ", array[i]);
 }
 return 0;
 }
