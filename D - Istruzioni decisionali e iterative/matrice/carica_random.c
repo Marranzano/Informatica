0665be9cbd489688b443966c7a36ff950a6111ba
Marranzano <leomarranzano@gmail.com>
2025-03-31 09:17:30 +0000

    carica

---
+++ D - Istruzioni decisionali e iterative/matrice/carica_random.c
@@ -0,0 +1,11 @@
+void carica_random(int m[][dim_c]), int dim_r, dim_c;
+#define r=5;
+#define c=7;
+int main()
+{
+int mat[r][c];
+carica_random(mat,r,c);
+
+
+}
+void carica_random(int m[][dim_c]), int dim_r, dim_c;)
