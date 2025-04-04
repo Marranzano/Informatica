f194ff0dbb339f699f04df8ba762e043e3905a66
Marranzano <leomarranzano@gmail.com>
2025-04-04 09:47:56 +0000

    coc

--- D - Istruzioni decisionali e iterative/matrice/carica_random.c
+++ D - Istruzioni decisionali e iterative/matrice/carica_random.c
@@ -1,11 +1,37 @@
-void carica_random(int m[][dim_c], int dim_r, dim_c)
-#define r=5;
-#define c=7;
-int main()
-{
-int mat[r][c];
-carica_random(mat,r,c);
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#define RIGHE 5   // Numero di righe
+#define COLONNE 7   // Numero di colonne
 
+void carica_random(int m[][COLONNE], int dim_r, int dim_c);
+void stampa(int m[][COLONNE], int dim_r, int dim_c)
+int main() {
+    int mat[RIGHE][COLONNE];
+    carica_random(mat, RIGHE, COLONNE);
+
+  
+
+    return 0;
 }
-void carica_random(int m[][dim_c], int dim_r, dim_c)
+
+// Funzione che carica valori casuali nella matrice
+void carica_random(int m[][COLONNE], int dim_r, int dim_c) {
+  
+    srand(time(NULL));
+
+    for (int i = 0; i < dim_r; i++) {
+        for (int j = 0; j < dim_c; j++) {
+            m[i][j] = rand() % 100;  
+        }
+    }
+}
+void stampa(int m[][COLONNE], int dim_r, int dim_c) {
+    for (int i = 0; i < dim_r; i++) {
+        for (int j = 0; j < dim_c; j++) {
+            printf("%2d ", m[i][j]);  // Stampa ogni elemento della matrice
+        }
+        printf("\n");  // Vai a capo dopo ogni riga
+    }
+}
